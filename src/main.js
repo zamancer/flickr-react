c962d2a50a13cdf105b1cf0c11e1c876484e5f81
Alan Zambrano <alnzam17@gmail.com>
2020-02-28 23:34:40 -0600

    Uses react as templating engine

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { compose, curry, map, prop } from 'ramda';
-import $ from 'jquery';
 import { url, requestResource } from './flickrApiClient';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // -- Utils ----------------------------------------------------------
 const Impure = {
@@ -8,17 +9,19 @@ const Impure = {
         console.log(tag, x);
         return x;
     }),
-    setHtml: curry((sel, html) => $(sel).html(html)),
+    setHtml: curry((sel, component) =>
+        ReactDOM.render(component, document.getElementById(sel))
+    ),
 };
 
 // -- Pure -----------------------------------------------------------
-const img = src => $('<img />', { src });
+const Img = src => <img key={src} src={src} />;
 const mediaUrl = compose(prop('m'), prop('media'));
 const mediaUrls = compose(map(mediaUrl), prop('items'));
-const images = compose(map(img), mediaUrls);
+const images = compose(map(Img), mediaUrls);
 
 // -- Impure ---------------------------------------------------------
-const render = compose(Impure.setHtml('#js-main'), images);
+const render = compose(Impure.setHtml('js-main'), images);
 const app = compose(requestResource(render), url);
 
 app('cats');
